07f27ee36343cc9a921b962194cdb5732b4296fd
ethenguo <ethen.guo@gmail.com>
2014-09-07 18:33:46 +0800

    Create lang.php

---
+++ lang/zh/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Chinese language file for flexpaper plugin
+ *
+ * @author Ethen Guo <Ethen.Guo@Gmail.com>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+// $lang['fixme'] = 'FIXME';
+
+
+
+//Setup VIM: ex: et ts=4 :
